b1a4d88c0a4d628a8402e4df40a15acbd2479d2c
PJyoti <bapunjyoti2004@gmail.com>
2024-09-01 00:50:34 +0530

    loading animation added

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./home.css";
 import Buttons from './Buttons';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulate loading time with useEffect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 8000); // Set the delay time as per your requirement
+    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="gradient-background  h-screen flex justify-center items-center">
+        <img src={'./safe-box.gif'} alt="Loading..." className="h-[100px] w-[100px] bg-black" />
+      </div>
+    );
+  }
   return (
     <div className="gradient-background h-screen flex flex-col items-center py-[8%] gap-10">
-      <div className="h-[50%] w-[45%] rounded-xl py-6 flex flex-col gap-10">
-        <div className="text-[50px] flex font-bungee font-bold items-center justify-center">
-          <span className="flex items-center h-10 text-orange-500 bg-clip-text">
-            <img src={'./icon.png'} alt="icon" className="h-[50px] w-[50px]" />
+      <div className="h-[50%] w-[90%] md:w-[60%] lg:w-[45%] rounded-xl py-6 flex flex-col gap-10">
+        <div className="text-[32px] sm:text-[40px] md:text-[50px] flex font-bungee font-bold items-center justify-center">
+          <span className="flex items-center h-10 w-50 text-orange-500 bg-clip-text">
+            <img src={'./icon.png'} alt="icon" className="h-[45px] w-[45px]" />
             PEN THE VAULT
           </span>
         </div>
