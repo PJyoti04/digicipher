0221409be36b6048f47d1956c094b42c18b22639
KC1064 <kiron08072003@gmail.com>
2024-09-01 10:58:43 +0530

    changes in icon and loading gif

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   if (isLoading) {
     return (
       <div className="gradient-background  h-screen flex justify-center items-center">
-        <img src={'./safe-box.gif'} alt="Loading..." className="h-[100px] w-[100px] bg-black" />
+        <img src={'./safe-box-wo-bg.gif'} alt="Loading..." className="h-[150px] w-[150px] bg-transparent" />
       </div>
     );
   }
@@ -25,7 +25,7 @@ const Home = () => {
       <div className="h-[50%] w-[90%] md:w-[60%] lg:w-[45%] rounded-xl py-6 flex flex-col gap-10">
         <div className="text-[32px] sm:text-[40px] md:text-[50px] flex font-bungee font-bold items-center justify-center">
           <span className="flex items-center h-10 w-50 text-orange-500 bg-clip-text">
-            <img src={'./icon.png'} alt="icon" className="h-[45px] w-[45px]" />
+            <img src={'./icon-no-bg.png'} alt="icon" className="h-[45px] w-[45px]" />
             PEN THE VAULT
           </span>
         </div>
