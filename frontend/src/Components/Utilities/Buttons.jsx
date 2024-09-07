a2a259c21d77eb2816eeef6011f8e482990e8de2
KC1064 <kiron08072003@gmail.com>
2024-09-07 13:20:02 +0530

    fixed the animations and improved it.

--- frontend/src/Components/Utilities/Buttons.jsx
+++ frontend/src/Components/Utilities/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Buttons = ({ props }) => {
   return (
-    <button className="relative border-0 cursor-pointer group w-[400px] ">
+    <button className="relative border-0 cursor-pointer group w-[400px]">
       <div className="relative overflow-hidden bg-red-600 py-4 px-20">
         <span className="absolute top-0 left-0 w-[120%] h-[110%] bg-gray-900 transform -translate-x-[105%] skew-x-[-10deg] transition-transform duration-[.8s] ease-in-out group-hover:translate-x-[-5%] group-hover:rounded-full"></span>
         <a href="#" className="relative text-2xl font-bold text-[#ece8e1] uppercase z-10">
