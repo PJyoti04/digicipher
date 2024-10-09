1ee2737126915d6ffec11ed139a6166807022efd
PJyoti <bapunjyoti2004@gmail.com>
2024-10-09 22:57:49 +0530

    Added FullScreen Mode

--- frontend/src/Components/Pages/Leaderboard.jsx
+++ frontend/src/Components/Pages/Leaderboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Navbar from '../utils/Navbar'
+import Footer from '../utils/Footer'
 
 const Leaderboard = () => {
   return (
-    <div>Leaderboard</div>
+    <div
+      className="h-[100vh] w-[100vw] flex gap-5 flex-col items-center bg-black"
+      style={{ backgroundImage: "url('./bg.svg')", backgroundSize: "cover" }}
+    >
+      <div className="h-full w-full bg-opacity-20 backdrop-blur-sm flex flex-col items-center gap-8">
+        <Navbar></Navbar>
+        <div className='h-[80vh]'></div>
+        <Footer></Footer>
+      </div>
+    </div>
   )
 }
 
