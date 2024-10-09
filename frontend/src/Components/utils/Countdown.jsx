d5bb55eabc10d7c865b10bc7a83b99c3019a470d
KC1064 <kiron08072003@gmail.com>
2024-10-09 22:22:09 +0530

    Did some fine tuning to the challenger mode

--- frontend/src/Components/utils/Countdown.jsx
+++ frontend/src/Components/utils/Countdown.jsx
@@ -2,29 +2,50 @@ import React, { useRef } from "react";
 import gsap from "gsap";
 
 const Countdown = ({ setStart }) => {
-  const btnRef = useRef();
+  const btnRef = useRef([]); // Initialize ref for buttons
 
   const handleStart = () => {
-    gsap.to(btnRef.current, {
+    gsap.to(btnRef.current[0], {
       x: -100,
-      y: -330,
+      y: -440,
       duration: 1,
-      opacity:0,
+      opacity: 0,
       onComplete: () => {
         setStart(false);
       },
     });
+
+    gsap.to(btnRef.current[1],{
+        x:100,
+        y:-440,
+        duration:1,
+        opacity:0,
+    })
   };
 
   return (
-    <div className="h-full z-50 bg-opacity-15 backdrop-blur-3xl w-full flex justify-center items-center">
-      <button
-        ref={btnRef}
-        className="text-white px-4 py-2 bg-green-400 rounded-lg"
-        onClick={handleStart} 
-      >
-        START
-      </button>
+    <div
+      style={{ fontFamily: "thunder" }}
+      className="h-full z-50 mt-24 bg-opacity-15 uppercase backdrop-blur-3xl w-full flex gap-20 flex-col items-center"
+    >
+      <h1 className="text-white text-3xl text-center">
+        Dare to Challenge: Decode the Mystery with Every Guess!
+      </h1>
+      <div className="flex flex-col w-full justify-center items-center gap-5">
+        <button
+          ref={(el) => (btnRef.current[0] = el)} 
+          className="font-bold w-[70%] px-4 py-2 text-black bg-green-400 rounded-lg text-2xl"
+          onClick={handleStart}
+        >
+          START
+        </button>
+        <button
+          ref={(el) => (btnRef.current[1] = el)} 
+          className="font-bold w-[70%] px-4 py-2 text-black bg-yellow-300 rounded-lg text-2xl" 
+        >
+          INSTRUCTIONS
+        </button>
+      </div>
     </div>
   );
 };
